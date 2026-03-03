c9bd0eca30d3188a8df121f8f627790a3ae8ff84
Daniel Soledad <danielalsole24@gmail.com>
2026-03-03 14:03:16 +0100

    Try update schema for light and dark icons

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,19 @@
 import { defineCollection, z } from "astro:content";
 
+const stackIconSchema = z.union([
+    z.string(),
+    z.object({
+        light: z.string(),
+        dark: z.string(),
+    }),
+]);
+
 const projects = defineCollection({
     schema: z.object({
         title: z.string(),
         img: z.string(),
         description: z.string(),
-        stack: z.array(z.string()),
+        stack: z.array(stackIconSchema),
         urls: z.object({
             demo: z.string().optional(),
             code: z.string().optional(),
