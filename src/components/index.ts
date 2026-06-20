1947a1275a9477da30c34fefbe1c2e333c763f53
DaAlexis <danielalsole24@gmail.com>
2026-06-20 20:29:41 +0200

    refactor: Components structure

---
+++ src/components/index.ts
@@ -0,0 +1,35 @@
+export { default as Header } from './layout/Header.astro';
+export { default as Footer } from './layout/Footer.astro';
+export { default as BackToTop } from './layout/BackToTop.astro';
+export { default as HeaderPage } from './layout/HeaderPage.astro';
+export { default as Breadcrumbs } from './layout/Breadcrumbs.astro';
+
+export { default as Hero } from './sections/Hero.astro';
+export { default as AboutMe } from './sections/AboutMe.astro';
+export { default as Experience } from './sections/Experience.astro';
+export { default as Projects } from './sections/Projects.astro';
+export { default as ContactMe } from './sections/ContactMe.astro';
+export { default as Hobbies } from './sections/Hobbies.astro';
+export { default as Education } from './sections/Education.astro';
+export { default as Blockquote } from './sections/Blockquote.astro';
+export { default as SoftSkillsRating } from './sections/SoftSkillsRating.astro';
+export { default as TechStack } from './sections/TechStack.astro';
+
+export { default as CardAboutMe } from './cards/CardAboutMe.astro';
+export { default as ExperienceCard } from './cards/ExperienceCard.astro';
+export { default as ProjectCard } from './cards/ProjectCard.astro';
+export { default as ProjectCardPage } from './cards/ProjectCardPage.astro';
+export { default as MyFavorites } from './cards/MyFavorites.astro';
+export { default as MyFavoritesCard } from './cards/MyFavoritesCard.astro';
+
+export { default as MusicPlayer } from './widgets/MusicPlayer.astro';
+
+export { default as Button } from './ui/Button.astro';
+export { default as Carousel } from './ui/Carousel.astro';
+export { default as ProjectFilter } from './ui/ProjectFilter.astro';
+
+export { default as GithubIcon } from './icons/GithubIcon.astro';
+export { default as LinkedinIcon } from './icons/LinkedinIcon.astro';
+
+export { default as Logo } from './media/Logo.astro';
+export { default as MyPhoto } from './media/MyPhoto.astro';
